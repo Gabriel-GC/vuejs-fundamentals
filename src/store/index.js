076b9e81c79f950c5e88f98e2031f9b53e3b4f4e
GABRIEL-GC <goncalves.cunhaa@gmail.com>
2024-12-17 16:52:17 -0300

    feat: adiciona sincronia com vuex

--- src/store/index.js
+++ src/store/index.js
@@ -10,7 +10,12 @@ export default createStore({
                 state.todos = payload
             },
             storeTodo(state, payload) {
-                state.todos.unshift(payload)
+                const index = state.todos.findIndex(todo => todo.id === payload.id)
+                if (index >= 0){
+                    state.todos.splice(index, 1, payload)
+                } else {
+                    state.todos.unshift(payload)
+                }
             }
         },
        
@@ -35,8 +40,10 @@ export default createStore({
                 })
             },
 
-            updateTodo(context, {id , data}){
-                return axios.put(`http://localhost:3000/todos/${id}`, data)
+            updateTodo({commit}, {id , data}){
+                return axios.put(`http://localhost:3000/todos/${id}`, data).then((response) => {
+                    commit('storeTodo', response.data)
+                })
             }
         },
 
